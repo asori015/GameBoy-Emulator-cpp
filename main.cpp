4026d25f03838e1de7184c5bba088ad06b65ce8a
asori015 <asori015@ucr.edu>
2022-08-26 03:47:20 -0700

    Adding screen component

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include "machine.h"
 
+class Screen{
+    public:
+     Screen(Machine* x);
+    private:
+     Machine* x;
+};
+
+Screen::Screen(Machine* x){
+    this->x = x;
+}
+
 void run(){
     std::string ROMPath = "D:\\Games\\GBA\\pokemon\\vanilla\\Pokemon red.gb";
     Machine* machine = new Machine("Hello GameBoy Emulator!");
-    machine->hello();
+    Screen* screen = new Screen(machine);
+    //screen.keypad = machine.getKeypad();
 }
 
 int main(){
