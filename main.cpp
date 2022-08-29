e030ffe56161cff2c04cb7bab0b6bb4ff14f60fa
asori015 <asori015@ucr.edu>
2022-08-29 05:14:26 -0700

    Played around with OpenGL for a bit. Found out its main job is to rasterize triangles in a 3d graphics pipeline, but it turns out it does have functions for direct pixel manipulation so I played around with that to see how opengl stores and reads pixels in a buffer.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <GL/glut.h>
 #endif
 
+#include <bitset>
+
 // class OneLoneCoder_Example: public olcConsoleGameEngine{
 //     public:
 //      bool OnUserCreate();
@@ -26,11 +28,39 @@ Screen::Screen(Machine* x){
     this->x = x;
 }
 
-void run(){
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    char buffer[160 * 144 * 3 + 8];
+
+    for(int i = 0; i < 144; i++){
+        for(int j = 0; j < 160 * 3; j++){
+            int index = (i * 160  * 3) + j;
+            buffer[index] = (index % 2) * 128;
+        }
+    }
+
+    glPixelZoom(4,4);
+    glDrawPixels(160, 144, GL_RGB, GL_UNSIGNED_BYTE, &buffer);
+    glFlush();
+}
+
+void run(int argc, char** argv){
     std::string ROMPath = "D:\\Games\\GBA\\pokemon\\vanilla\\Pokemon red.gb";
     Machine* machine = new Machine("Hello GameBoy Emulator!");
     Screen* screen = new Screen(machine);
     //screen.keypad = machine.getKeypad();
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+    glutInitWindowPosition(150, 150);
+    glutInitWindowSize(160 * 4, 144 * 4);
+    glutCreateWindow("A Simple Triangle");
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
 
     // OneLoneCoder_Example game;
     // game.ConstructConsole(160, 100, 8, 8);
@@ -38,7 +68,7 @@ void run(){
 
 }
 
-int main(){
-    run();
+int main(int argc, char** argv){
+    run(argc, argv);
     return 0;
 }
